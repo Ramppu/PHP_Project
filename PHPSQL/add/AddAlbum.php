26033c2c29505fe132f66019adb5e158e652b02d
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-05-02 21:19:18 +0300

    Update AddAlbum.php

--- PHPSQL/add/AddAlbum.php
+++ PHPSQL/add/AddAlbum.php
@@ -5,38 +5,30 @@
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="Expires" content="-1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="../css/mystyleTwo.css">
+<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
 </head>
 <body>
 
-<div class="Header">
-     <h1><a href="../WebPage.php">Lissen</a></h1>      
+<div class="Header2">
+     <h2><a href="../WebPage.php">Lissen</a></h2>      
 </div>
-
-
-
 <form action="albumconfirm.php" method="get" autocomplete = "off">
     <label for = "Artist">
-        Artist 
-         <input type="text" name="Artist">
+         <input type="text" placeholder="Artist" name="Artist">
     </label>    
    <label for = "Album Name">
-        Album Name 
-         <input type="text" name="Album">
+         <input type="text" placeholder="Album Name" name="Album">
     </label>
     <label for = "Release Date">
-        Release Date
          <input type="date" name="Date">
     </label>
     <label for = "Additional Information">
-        Additional Information 
-         <input type="text" name="Info">
+         <input type="text" placeholder="Additional Info" name="Info">
     </label>
     <label>   
     <input type="submit" value="Add An Album">
     </label>
 </form>
-
-
+     
 </body>
 </html> 
