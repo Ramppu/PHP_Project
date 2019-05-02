fa89efc6328a6549e2dc0aedc2184255a50d1014
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-05-02 21:27:27 +0300

    Update Delete.php

--- PHPSQL/delete/Delete.php
+++ PHPSQL/delete/Delete.php
@@ -5,26 +5,24 @@
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="Expires" content="-1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="../css/mystyleTwo.css">
+<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
 </head>
 <body>
 
-<div class="Header">
-     <h1><a href="../WebPage.php">Lissen</a></h1>      
+<div class="Header2">
+     <h2><a href="../WebPage.php">Lissen</a></h2>      
 </div>
 
 
 
 <form action="delConfirm.php" method="get" autocomplete = "off">   
    <label for = "Album Name">
-        Album Name 
-         <input type="text" name="Album">
+         <input type="text" placeholder="Album name" name="Album">
     </label>
     <label>   
     <input type="submit" value="Delete an Album.">
     </label>
 </form>
-
 
 </body>
 </html> 
