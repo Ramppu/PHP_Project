8849f9279d97bf0c90439a613edaf569b875d345
Ramppu <46961429+Ramppu@users.noreply.github.com>
2019-05-02 21:50:58 +0300

    Update RegistrationConfirm.php

--- PHPSQL/registration/RegistrationConfirm.php
+++ PHPSQL/registration/RegistrationConfirm.php
@@ -1,16 +1,17 @@
 <!doctype html>
 <html>
 <head>
-<title>Gelor | Search for Albums</title>
+<title>Lissen | Search for Albums</title>
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="../css/mystyleTwo.css">
+<link rel="stylesheet" type="text/css" href="../css/mystyle.css">
 <?php
 require '../Config.php';
 ?>
 </head>
 <body>
-
+    
+<div class="crazy">
 <?php
     $User = $_POST['Username'];
     $Email = $_POST['email'];
@@ -45,5 +46,6 @@ require '../Config.php';
 
     }
 ?>
-
+</div>
 </body>
+
